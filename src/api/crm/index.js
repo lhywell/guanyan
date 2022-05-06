c9e2517d36db4f07ee8ac26462a8880dc1d8d6f6
lhywell <lihuiyin128@163.com>
2022-05-06 17:19:45 +0800

    销售列表接口

--- src/api/crm/index.js
+++ src/api/crm/index.js
@@ -2,118 +2,37 @@
 import request from '@/common/axios/index'
 import { baseURL } from '@/config'
 
-// 分页查询新闻列表
-export async function getNews(data = {}) {
+// 得到销售列表
+export async function getSaleList(data = {}) {
   return request({
-    url: `${baseURL}/news/page`,
-    method: 'post',
-    data,
-  })
-}
-
-// 分页查询频道列表
-export async function getChannel(data = {}) {
-  return request({
-    url: `${baseURL}/channel/list`,
+    url: `${baseURL}/sale/list`,
     method: 'get',
     data,
   })
 }
 
-// 更新新闻
-export async function newsUpdate(data = {}) {
+// 分页查询客户列表
+export async function getCustomer(data = {}) {
   return request({
-    url: `${baseURL}/news/update`,
+    url: `${baseURL}/customer/page`,
     method: 'post',
     data,
   })
 }
 
-// 售电公司介绍编辑
-export async function sellCompanyUpdate(data = {}) {
+// 添加客户
+export async function addCustomer(data = {}) {
   return request({
-    url: `${baseURL}/sellCompanyIntroduce/update`,
+    url: `${baseURL}/customer/add`,
     method: 'post',
     data,
   })
 }
 
-// 获取已存在的区域列表
-export async function getMarketAreas(data = {}) {
+// 下载
+export async function downloadExcel(data = {}) {
   return request({
-    url: `${baseURL}/marketInfo/areas`,
-    method: 'get',
-    data,
-  })
-}
-
-// 创建市场信息
-export async function createMarket(data = {}) {
-  return request({
-    url: `${baseURL}/marketInfo/create`,
-    method: 'get',
-    data,
-  })
-}
-
-// 删除市场信息
-export async function deleteMarket(data = {}) {
-  return request({
-    url: `${baseURL}/marketInfo/delete`,
-    method: 'get',
-    data,
-  })
-}
-
-// 市场信息更新
-export async function updateMarket(data = {}) {
-  return request({
-    url: `${baseURL}/marketInfo/update`,
-    method: 'post',
-    data,
-  })
-}
-
-// 分页查询售电公司
-export function getCompany(data) {
-  return request({
-    url: `${baseURL}/sellCompany/page`,
-    method: 'post',
-    data,
-  })
-}
-
-// 创建售电公司
-export function createCompany(data) {
-  return request({
-    url: `${baseURL}/sellCompany/create`,
-    method: 'post',
-    data,
-  })
-}
-
-// 售电公司信息更新
-export function updateCompany(data) {
-  return request({
-    url: `${baseURL}/sellCompany/update`,
-    method: 'post',
-    data,
-  })
-}
-
-// 分页获取售电公司列表
-export function getSellCompanyIntroduce(data) {
-  return request({
-    url: `${baseURL}/sellCompanyIntroduce/page`,
-    method: 'post',
-    data,
-  })
-}
-
-// 售电公司介绍审核
-export function sellCompanyExamine(data) {
-  return request({
-    url: `${baseURL}/sellCompanyIntroduce/examine`,
+    url: `${baseURL}/customer/add`,
     method: 'post',
     data,
   })
