30bf585610c017dd8086dd596bead4549c80bfc0
lhywell <lihuiyin128@163.com>
2022-05-22 09:47:59 +0800

    去掉

--- src/common/mixin/common.js
+++ src/common/mixin/common.js
@@ -138,18 +138,6 @@ export default {
         ],
       }
     },
-    getStatus(status) {
-      if (status === '1') {
-        return '待审核'
-      }
-      if (status === '2') {
-        return '审核通过'
-      }
-      if (status === '3') {
-        return '审核驳回'
-      }
-      return '待审核'
-    },
     getIndex($index, pageNo, pageSize) {
       return $index + (pageNo - 1) * pageSize + 1
     },
